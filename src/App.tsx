06d768140fb48f9dff8f10442ae6b9c02968a65e
Sergi kaliashvili <kaliashvilisergi@gmail.com>
2025-01-04 23:43:00 +0400

    Initial commit with React app

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,61 @@
+import { ChangeEvent, useState,useReducer } from "react"
 //usereducer gamoikeneba ert logikashi gaertianebul
 //ramdenime statis shesacvlelad da gasaertianeblad
-import { reducer, initialState } from "./reducer"
-import { ChangeEvent,  useReducer } from "react"
-import Child from "./Child"
-  //count statis shecvlislogika ikneba ak, da gamovidzaebt dispatchit
-  //actionit veubnebit rogor sheicvalos statis mnishvneloba daakldes tu gaizardos
-
-
-function App() {
- 
-  const [state, dispatch] =  useReducer(reducer, initialState)  
-
-  const {count,step} = state
-  
-  //am cvladshi purbelielementi aris state,anu is mnishveloba romlitac chveni
-  // logika kontroldeba mag count, dispatch ukve funqciaa ritac shevidzlia
-  //gamovidzaxod reducer funqcia
-
-  //  usereducershi pirveli argumenti funqciaa,meore sawkisi mnishvnelobaa
-  // const [count, setCount] = useState(0)
-
-  const date = new Date("2021-09-21")
-  date.setDate(date.getDate() + count)
-
-  function defineCount(event: ChangeEvent<HTMLInputElement>){
-    dispatch( {type: "setCount", payload: Number(event.target.value) } )
+//count statis shecvlislogika ikneba ak, da gamovidzaebt dispatchit
+ // //actionit veubnebit rogor sheicvalos statis mnishvneloba daakldes tu gaizardos
+function reducer(state,action){
+  if(action.type === 'inc'){
+    return state + 1
   }
-  
-  function defineStep(event: ChangeEvent<HTMLInputElement>){
-      dispatch({type:"setStop", payload: Number(event.target.value)})
+  if(action.type === 'dec'){
+    return state - 1
   }
-
-  function inc(){
-    dispatch({type : "inc",}  )//payload 1 anu gaizardos 1it
-  }
-  function dec(){
-    dispatch({type : "dec",  }  )
-
-  }
-
-function reset(){
-  dispatch({type:'reset'})
-  // setStep(1)
 }
+function App() {
+  //dispatch aris funqcia, countki state aris ra,magit vakontrolebt logikas
+  // am shemtxevavshi count aris 0,reducer funqciashi mekneba states shecvlis
+  //logika,am funqcias ki gamovidzaxebt dispatchit
+  const initialState = 0
+  const [count,dispatch] = useReducer(reducer,initialState)
+//  const [count , setCount] = useState(0)
+ const  [step , setStep] = useState(1)
+const date = new Date("2021-09-01")
+date.setDate(date.getDate()+ count)
+function defineCount(event: ChangeEvent<HTMLInputElement>){
+  // setCount(Number(event.target.value))
+}
+function defineStep(event: ChangeEvent<HTMLInputElement>){
+  setStep(Number(event.target.value))
+}
+function inc(){
+  // setCount((prev)=> prev + step)
+  dispatch({type: 'inc'})
+}
+function dec(){
+  dispatch({type:'dec'})
+  // setCount((prev)=> prev-step)
+}
+function reset(){
+  // setCount(0)
+  setStep(0)
+}
+
   return (
    <div>
     <div>
-      <input type="range" onChange={defineStep} min={1} max={10} />{step}
+      <input type="range" onChange={defineStep} min={1} max={10} />
+      <div>
+        <button onClick={dec}>-</button>
+        <input type="number" value={count} onChange={defineCount} />
+        <button onClick={inc}>+</button>
+      </div>
+      {date.toString()}
     </div>
     <div>
-      <button onClick={dec}></button>
-      <input type="number" value={count} onChange={defineCount} />
-      <button onClick={inc}>+</button>
+    {" "}
+    <button onClick={reset}>Reset</button>
     </div>
-    {date.toString()}
-    <div>
-      {" "}
-      <button onClick={reset}>reset</button>
-    </div>
-    <Child dispatch = {dispatch}  />
+
    </div>
   )
 }
